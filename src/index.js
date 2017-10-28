71737deac1dfec1e46718f7bac8199671d991a9d
JGAntunes <j.goncalo.antunes@gmail.com>
2017-10-28 15:03:30 +0100

    clean code and better bundling

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ const SetKeys = require('./states/set-keys')
 const PreloadGame = require('./states/preload-game')
 const GameMananger = require('./states/game-manager')
 
+require('./network')
+
 // TODO clear this
 window.baseW = 1366
 window.baseH = 768
@@ -26,7 +28,7 @@ const winRatio = winW / winH
 const height = Math.round(Math.sqrt(window.baseArea / winRatio))
 const width = Math.round(winRatio * height)
 
-const game = new Phaser.Game(width, height, Phaser.CANVAS, '')
+const game = new window.Phaser.Game(width, height, window.Phaser.CANVAS, '')
 
 game.state.add('Boot', Boot)
 game.state.add('PreloadMenu', PreloadMenu)
@@ -36,3 +38,46 @@ game.state.add('SetKeys', SetKeys)
 game.state.add('PreloadGame', PreloadGame)
 game.state.add('GameMananger', GameMananger)
 game.state.start('Boot')
+
+window.buttonDown = function buttonDown () {
+  this.tweenOut.onComplete.active = true
+  this.tween.start()
+}
+
+window.buttonUp = function buttonUp () {
+  this.tween.start()
+}
+
+window.clickButton = function clickButton (button, callback, state) {
+  var s = button.scale
+  var tweenTime = 30
+  if (window.mobile) {
+    tweenTime = 80
+  }
+  var tweenIn = button.game.add.tween(s).to({ x: s.x * 0.85, y: s.y * 0.85 }, tweenTime, window.Phaser.Easing.Linear.None, false)
+  var tweenOut = button.game.add.tween(s).to({ x: s.x, y: s.y }, tweenTime, window.Phaser.Easing.Linear.None, false)
+
+  tweenOut.onComplete.add(function () {
+    if (!tweenOut.isRunning && !tweenIn.isRunning) {
+      callback.call(this)
+    }
+  }
+  , state)
+
+  button.onInputOver.add(function () {
+    tweenOut.onComplete.active = true
+  })
+
+  button.onInputOut.add(function () {
+    tweenOut.onComplete.active = false
+  })
+
+  button.onInputDown.add(window.buttonDown, {
+    tween: tweenIn,
+    tweenOut: tweenOut
+  })
+
+  button.onInputUp.add(window.buttonUp, {
+    tween: tweenOut
+  })
+}
